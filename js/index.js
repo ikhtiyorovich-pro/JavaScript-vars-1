ad1328426e2d7ceb2a4144531057179eb0953696
ikhtiyorovich-pro <muhammadali.fayzullayev@yandex.ru>
2021-09-07 12:51:17 +0500

    next changes

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var intro = alert('Xush kelibsiz!')
+
+var intro = alert("Bizning kompaniyamizni tanlaganingiz uchun rahmat!")
+
+var firstName = prompt("Ismingizni kiriting:");
+var surName = prompt("Familiyangizni kiriting:");
+var age = prompt("Yoshingizni kiriting:");
+var aboutYou = prompt("O'zingiz haqingizda qisqacha ma'lumot");
+var feedBack = prompt("Nima sababdan bizning kompaniyamizda ishlamoqchisiz?");
+var exitBye = prompt("Arizanigiz qabul qilindi😊");
+
+
+console.log(`Ismingiz - ${firstName}\nFamiliyangiz - ${surName}\nYoshingiz - ${age}\nO'zingiz haqingizda qisqacha ma'lumot - ${aboutYou}\nNima sababdan bizning kompaniyamizda ishlamoqchisiz? - ${feedBack}\nArizanigiz qabul qilindi😊 - ${exitBye}`);
